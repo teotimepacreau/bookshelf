3a9bf276c4b1cda61d343bbf9deb2c7c2cdc7e18
teotimepacreau <95431443+teotimepacreau@users.noreply.github.com>
2023-12-18 10:13:11 +0100

    build(sort AZ)

--- frontend/frontJS/sort.js
+++ frontend/frontJS/sort.js
@@ -1,33 +1,38 @@
-// PSEUDO CODE : au clic sur l'option je récupère la valeur de l'option. EN fonction de la valeur de l'option j'exécute un ordre.
 
-const sorter = ()=>{
-    const allBooks = Array.from(document.querySelectorAll('.book-card'))//Array.from car sort ne fonctionne pas sur une NodeList
-    console.log(allBooks)
+const sorter = () => {
+  const allBooks = Array.from(document.querySelectorAll(".book-card")); //Array.from car sort ne fonctionne pas sur une NodeList
+  console.log(allBooks);
 
-    const sorter = document.querySelector('select[name="sort-by"]')
-    sorter.addEventListener('change', (e)=> {
-        console.log(e.target.value)
-        const value = e.target.value
-        switch(value){
-            case 'Titre de A à Z':
-                console.log('case1')
-                const AZArray = allBooks.toSorted((a,b)=>{
-                    const titleA = a.querySelector('.book-title').innerText
-                    const titleB = b.querySelector('.book-title').innerText
-                    return titleA.localeCompare(titleB)
-                })
-                console.log(AZArray)
-                
-                break
-            case "Titre de Z à A":
-                console.log('case2')
-                break
-            case "Note croissante":
-                break
-            case "Note décroissante":
-                break
-        }
+  const containerForAllCards = document.querySelector(
+    "[data-container-for-all-cards]"
+  );
+
+  const sorter = document.querySelector('select[name="sort-by"]');
+  sorter.addEventListener("change", (e) => {
+    console.log(e.target.value);
+    const value = e.target.value;
+    switch (value) {
+      case "Titre de A à Z":
+        console.log("case1");
+        allBooks.sort((a, b) => {
+          const titleA = a.querySelector(".book-title").innerText;
+          const titleB = b.querySelector(".book-title").innerText;
+          return titleA.localeCompare(titleB);
+        });
+        break;
+      case "Titre de Z à A":
+        console.log("case2");
+        break;
+      case "Note croissante":
+        break;
+      case "Note décroissante":
+        break;
+    }
+    // chaque case retourne l'array allBooks mutée à sa manière, il ne reste plus qu'à vider le HTML du container et le remplir avec l'array mutée
+    containerForAllCards.innerHTML = ""
+    allBooks.forEach((book)=>{
+        containerForAllCards.appendChild(book)
     })
+})
 }
-
-export default sorter
+export default sorter;
