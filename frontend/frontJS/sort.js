41fee822e73dde811aef92b5ad97aa57eadc82c5
Teotime Pacreau <teotime.pac@outlook.fr>
2023-12-18 11:46:40 +0100

    build(sorter)

--- frontend/frontJS/sort.js
+++ frontend/frontJS/sort.js
@@ -1,6 +1,7 @@
-
 const sorter = () => {
-  const allBooks = Array.from(document.querySelectorAll(".book-card")); //Array.from car sort ne fonctionne pas sur une NodeList
+  let initialBooksArray = Array.from(document.querySelectorAll(".book-card")); //Array.from car sort ne fonctionne pas sur une NodeList
+
+  let result = undefined; //on ouvre le let result pour pouvoir ensuite le remplit avec toSorted() pour ne pas muter l'array initiale
 
   const containerForAllCards = document.querySelector(
     "[data-container-for-all-cards]"
@@ -10,43 +11,50 @@ const sorter = () => {
   sorter.addEventListener("change", (e) => {
     const value = e.target.value;
     switch (value) {
+      case "Trier-defaut":
+        result = initialBooksArray;
+        break;
       case "Titre de A à Z":
-        allBooks.sort((a, b) => {
+        result = initialBooksArray.toSorted((a, b) => {
           const titleA = a.querySelector(".book-title").innerText;
           const titleB = b.querySelector(".book-title").innerText;
           return titleA.localeCompare(titleB);
         });
         break;
       case "Titre de Z à A":
-        allBooks.sort((a,b)=>{
+        result = initialBooksArray.toSorted((a, b) => {
           const titleA = a.querySelector(".book-title").innerText;
           const titleB = b.querySelector(".book-title").innerText;
-          return titleA.localeCompare(titleB)
-        })
-        allBooks.reverse()
+          return titleB.localeCompare(titleA);
+        });
         break;
       case "Note croissante":
-        console.log('case3')
-        allBooks.sort((a,b)=>{
-          const averageRatingA = parseFloat(a.querySelector('[data-averagerating]').innerText);//obligé de parseFloat car sinon compare les string au lieu des nb
-          const averageRatingB = parseFloat(b.querySelector('[data-averagerating]').innerText);
-          return averageRatingA - averageRatingB
-        })
+        result = initialBooksArray.toSorted((a, b) => {
+          const averageRatingA = parseFloat(
+            a.querySelector("[data-averagerating]").innerText
+          ); //obligé de parseFloat car sinon compare les string au lieu des nb
+          const averageRatingB = parseFloat(
+            b.querySelector("[data-averagerating]").innerText
+          );
+          return averageRatingA - averageRatingB;
+        });
         break;
       case "Note décroissante":
-        allBooks.sort((a,b)=>{
-          const averageRatingA = parseFloat(a.querySelector('[data-averagerating]').innerText);//obligé de parseFloat car sinon compare les string au lieu des nb
-          const averageRatingB = parseFloat(b.querySelector('[data-averagerating]').innerText);
-          return averageRatingA - averageRatingB
-        })
-        allBooks.reverse()
+        result = initialBooksArray.toSorted((a, b) => {
+          const averageRatingA = parseFloat(
+            a.querySelector("[data-averagerating]").innerText
+          ); //obligé de parseFloat car sinon compare les string au lieu des nb
+          const averageRatingB = parseFloat(
+            b.querySelector("[data-averagerating]").innerText
+          );
+          return averageRatingB - averageRatingA;
+        });
         break;
     }
-    // chaque case retourne l'array allBooks mutée à sa manière, il ne reste plus qu'à vider le HTML du container et le remplir avec l'array mutée
-    containerForAllCards.innerHTML = ""
-    allBooks.forEach((book)=>{
-        containerForAllCards.appendChild(book)
-    })
-})
-}
+    // chaque case retourne l'array result changée à sa manière, sans toucher à l'initialBooksArray, il ne reste plus qu'à vider le HTML du container et le remplir avec result    containerForAllCards.innerHTML = "";
+    result.forEach((book) => {
+      containerForAllCards.appendChild(book);
+    });
+  });
+};
 export default sorter;
